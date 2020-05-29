5bdb0ea9e8f5d0a8da996afb5ae5c103ff186ea2
zagoksu <zeynelgoksu@gmail.com>
2020-05-30 01:55:18 +0200

    project week2 update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import './App.css';
-import CityCard from './CityCard';
-import weatherList from './city-weather.json'
+import Forecast from './Components/Forecast';
 
-function App() {
+export default function App() {
+
   return (
     <div className="App">
-      <h1> Weather </h1>
-{
-    weatherList.map((city) => {
-        return (
-          <CityCard
-            name={city.name}
-            countryCode={city.sys.country}
-            mainWeather={city.weather[0].main}
-            description={city.weather[0].description}
-            temp_min={city.main.temp_min}
-            temp_max={city.main.temp_max}
-            longitude={city.coord.lon}
-            latitude={city.coord.lat}
-          />
-        );
-      })}
+      <Forecast />
     </div>
   );
+
   
 }
 
-export default App;
